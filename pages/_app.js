a68608535cd9a1e5ebc9a11616a587eccad05f68
Vipin Das <118341086+vipinsatlok@users.noreply.github.com>
2023-03-23 13:19:15 +0530

    Add : Adsence Code in my site

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,15 @@
 import Footer from "@/components/Footer";
 import Header from "@/components/Header";
+import Head from "next/head";
 import "@/styles/globals.css";
 
 export default function App({ Component, pageProps }) {
   return (
     <div className="flex bg-gray-900 flex-col min-h-screen">
+    <Head>
+      <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5828945217071181"
+       crossorigin="anonymous"></script>
+    </Head>
       <header className="min-h-max bg-blue-500 flex items-center py-5 px-5">
         <Header />
       </header>
